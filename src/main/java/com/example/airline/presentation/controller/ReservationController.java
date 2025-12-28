759b93abb5ee3ce157cf9d29ad9d5dc76524c329
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-12-28 13:15:50 +0300

    added controller

--- src/main/java/com/example/airline/presentation/controller/ReservationController.java
+++ src/main/java/com/example/airline/presentation/controller/ReservationController.java
@@ -1,4 +1,45 @@
 package com.example.airline.presentation.controller;
+//Controller = The adapter between the external world and the system
 
+import com.example.airline.business.service.ReservationService;
+import com.example.airline.common.dto.CreateReservationRequest;
+import com.example.airline.common.dto.ReservationResponse;
+import com.example.airline.domain.entities.Reservation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservations")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody CreateReservationRequest request){
+        Reservation reservation = reservationService
+                .createReservation(
+                        request.getFlightId(),
+                        request.getPassengerId(),
+                        request.getSeatType()
+        );
+
+        ReservationResponse response = new ReservationResponse(
+                reservation.getId(),
+                reservation.getFlight().getFlightNumber(),
+                reservation.getSeatType(),
+                reservation.getPrice().getAmount(),
+                reservation.getPrice().getCurrency()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
 }
